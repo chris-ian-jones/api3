2a90b473788bbaffec6c0820c1baef17d29c2c25
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-04 18:44:51 +0100

    WIP: added Products Router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 
+const productsRouter = require('../products/productsRouter')
+
 const server = express()
+
+// global middleware
+
+server.use(express.json())
+
+server.use('/products', productsRouter)
+// server.use('/suppliers', suppliersRouter)
+
+// routes
 
 server.get('/', (req, res) => {
   res.status(200).json({api: 'up'})
