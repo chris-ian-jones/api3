9e1a24281c7fd74959579ee5b3e3b274f26bdd6b
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-04 18:49:09 +0100

    WIP: added Suppliers Router

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const productsRouter = require('../products/productsRouter')
+const suppliersRouter = require('../suppliers/suppliersRouter')
 
 const server = express()
 
@@ -9,7 +10,7 @@ const server = express()
 server.use(express.json())
 
 server.use('/products', productsRouter)
-// server.use('/suppliers', suppliersRouter)
+server.use('/suppliers', suppliersRouter)
 
 // routes
 
